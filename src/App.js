9efaee43964a5ec1ada630d2c90df4a242411917
santhosh <you@exapmle.com>
2022-04-09 09:27:43 +0530

    added local img in local repo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>this heading is from local repository</h1>
       <h1> this heading is from remote repository</h1>
     <img src="https://img.jagranjosh.com/images/2022/February/1822022/Chhatrapati-Shivaji-Maharaj-biography.webp"></img>
+     <img src="https://filmfare.wwmindia.com/content/2020/oct/rajnikanthearned201911604066189.jpg" alt="img from the local repo"></img>
     </div>
   );
 }
